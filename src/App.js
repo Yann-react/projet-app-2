a634ac6c27eecba1c27782392950256f8955d942
Lewis Alvigno Balogou <Lewisalvi@gmail.com>
2022-05-23 16:43:56 +0000

    ajout page nos sites 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import Landing from "./Components/Landing";
 import './App.css'
 import Sites from "./Components/Sites";
-import NosSites from "./Components/PageSites/NosSites";
 function App() {
   const resp = true;
   return (
     <div className="App">
    <div className="page" >
 
-    {/* <Landing />
+    <Landing />
 
     <Sites />
- */}
-<NosSites />
+
+
    </div>
    
     
